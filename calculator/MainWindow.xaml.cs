76d7b8448a0398c47f2b201bb07e027534d0d917
nitay <nitay@DESKTOP-UP7TDND>
2023-12-04 21:57:05 +0200

    Add project files.

---
+++ calculator/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        internal class start
+        {
+
+        }
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            string name = NameTextBox.Text;
+            string age = AgeTextBox.Text;
+            MainWindow gamepage = new MainWindow();
+            gamepage.Show();
+            this.Close();
+        }
+    }
+}
---
+++ calculator/mathgame.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace calculator
+{
+    public partial class mathgame : Window
+    {
+        private int number1;
+        private int number2;
+        private int correctAnswer;
+        private Random random = new Random();
+
+        public mathgame()
+        {
+            InitializeComponent();
+            GenerateNewProblem();
+        }
+
+        private void GenerateNewProblem()
+        {
+            number1 = random.Next(1, 10);
+            number2 = random.Next(1, 10);
+            correctAnswer = number1 + number2; // Example for addition
+
+            // Update UI elements here to show the problem
+            // e.g., questionTextBlock.Text = $"{number1} + {number2} = ?";
+        }
+        private void txtNumber1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Add logic here if needed
+        }
+
+        private void txtNumber2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Add logic here if needed
+        }
+
+        private void CheckAnswer()
+        {
+            // Assuming answerTextBox is a TextBox where the user enters their answer
+            if (int.TryParse(answerTextBox.Text, out int userAnswer))
+            {
+                if (userAnswer == correctAnswer)
+                {
+                    MessageBox.Show("Correct!");
+                }
+                else
+                {
+                    MessageBox.Show("Incorrect. Try again.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number.");
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CheckAnswer();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
